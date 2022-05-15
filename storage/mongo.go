ba1d8803169d2b945bee3b0db7170224c6beb539
jasper-zsh <jasper.zsh@hotmail.com>
2022-05-16 02:06:04 +0800

    improve

--- storage/mongo.go
+++ storage/mongo.go
@@ -4,6 +4,7 @@ import (
 	"dht-ocean/bittorrent"
 	"dht-ocean/model"
 	"github.com/kamva/mgm/v3"
+	"github.com/sirupsen/logrus"
 )
 
 var _ bittorrent.TorrentHandler = (*MongoTorrentHandler)(nil)
@@ -12,5 +13,11 @@ type MongoTorrentHandler struct{}
 
 func (m MongoTorrentHandler) HandleTorrent(torrent *bittorrent.Torrent) {
 	t := model.NewTorrentFromCrawler(torrent)
-	_ = mgm.Coll(t)
+	col := mgm.Coll(t)
+	err := col.Create(t)
+	if err != nil {
+		logrus.Errorf("Failed to save torrent %s %s %v", t.InfoHash, t.Name, err)
+	} else {
+		logrus.Infof("Saved torrent %s %s", t.InfoHash, t.Name)
+	}
 }
