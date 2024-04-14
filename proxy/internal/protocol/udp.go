c06e8fe9fb4805ae8935f17aec8e40b4fec7f735
jasper-zsh <jasper.zsh@hotmail.com>
2024-04-14 17:15:44 +0800

    add: header checksum

--- proxy/internal/protocol/udp.go
+++ proxy/internal/protocol/udp.go
@@ -1,6 +1,7 @@
 package protocol
 
 import (
+	"bytes"
 	"encoding/binary"
 	"io"
 	"net/netip"
@@ -8,26 +9,64 @@ import (
 	"github.com/juju/errors"
 )
 
+var UDPHeaderSize = binary.Size(UDPHeader{})
+
 type UDPHeader struct {
 	Version byte
 	AddrHeader
-	Length uint32
+	Length   uint32
+	Checksum byte
+}
+
+func checksum(data []byte, length int) {
+	data[length-1] = 0
+	t := 0
+	for _, v := range data {
+		t += int(v)
+	}
+	data[length-1] = byte(t % 0xFF)
 }
 
 func (h *UDPHeader) WriteTo(writer io.Writer, addr []byte) error {
-	err := binary.Write(writer, binary.BigEndian, h)
+	h.Checksum = 0
+	b := bytes.NewBuffer(make([]byte, 0, UDPHeaderSize))
+	err := binary.Write(b, binary.BigEndian, h)
 	if err != nil {
 		return errors.Trace(err)
 	}
-	_, err = writer.Write(addr)
+	data := b.Bytes()
+	checksum(data, len(data))
+	n, err := writer.Write(data)
 	if err != nil {
 		return errors.Trace(err)
+	}
+	if n != len(data) {
+		return io.ErrShortWrite
+	}
+	n, err = writer.Write(addr)
+	if err != nil {
+		return errors.Trace(err)
+	}
+	if n != len(addr) {
+		return io.ErrShortWrite
 	}
 	return nil
 }
 
 func (h *UDPHeader) ReadFrom(reader io.Reader) (addrPort netip.AddrPort, err error) {
-	err = binary.Read(reader, binary.BigEndian, h)
+	raw := make([]byte, UDPHeaderSize)
+	_, err = io.ReadFull(reader, raw)
+	if err != nil {
+		err = errors.Trace(err)
+		return
+	}
+	c := raw[len(raw)-1]
+	checksum(raw, len(raw))
+	if c != raw[len(raw)-1] {
+		err = errors.New("checksum not match")
+		return
+	}
+	err = binary.Read(bytes.NewReader(raw), binary.BigEndian, h)
 	if err != nil {
 		err = errors.Trace(err)
 		return
